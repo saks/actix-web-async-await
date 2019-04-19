7bb6a89e5b2f592f71a67a1a4c0a154184a0749e
Aliaksandr Rahalevich <saksmlz@gmail.com>
2019-04-18 21:26:49 -0700

    attempt to make it work with latest actix-web

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
 #![feature(await_macro, futures_api, async_await)]
 
+use actix_web::error::Error;
+use futures::Future as Future01;
 use std::future::Future;
 use tokio_async_await::compat::backward::Compat;
-use futures::Future as Future01;
-use actix_web::{FutureResponse, error::Error};
+
+/// Convenience type alias
+type FutureResponse<I, E = Error> = Box<Future01<Item = I, Error = E>>;
 
 // Re-export `tokio::await` for ease-of-use
 pub use tokio_async_await::await;
@@ -26,5 +29,27 @@ define_compat!(compat(arg1: Arg1));
 define_compat!(compat2(arg1, arg2: Arg1, Arg2));
 define_compat!(compat3(arg1, arg2, arg3: Arg1, Arg2, Arg3));
 define_compat!(compat4(arg1, arg2, arg3, arg4: Arg1, Arg2, Arg3, Arg4));
-define_compat!(compat5(arg1, arg2, arg3, arg4, arg5: Arg1, Arg2, Arg3, Arg4, Arg5));
-define_compat!(compat6(arg1, arg2, arg3, arg4, arg5, arg6: Arg1, Arg2, Arg3, Arg4, Arg5, Arg6));
+define_compat!(compat5(
+    arg1,
+    arg2,
+    arg3,
+    arg4,
+    arg5: Arg1,
+    Arg2,
+    Arg3,
+    Arg4,
+    Arg5
+));
+define_compat!(compat6(
+    arg1,
+    arg2,
+    arg3,
+    arg4,
+    arg5,
+    arg6: Arg1,
+    Arg2,
+    Arg3,
+    Arg4,
+    Arg5,
+    Arg6
+));
